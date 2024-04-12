2e95c161fbaa49b0edd186ddc283324d72e7ff4d
AmirMohammad Molazemi <amirmohamad.molazemi@gmail.com>
2024-04-12 19:58:17 +0330

    fix:fix the bug

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import paths from "./src/constants/paths";
 
-const paths = [
-  "src",
-  "assets",
-  "components",
-  "configs",
-  "pages",
-  "router",
-  "services",
-  "styles",
-  "utils",
-];
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
