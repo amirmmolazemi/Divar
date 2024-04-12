97c41b19f44323c7414f495b90334acaf44bd017
AmirMohammad Molazemi <amirmohamad.molazemi@gmail.com>
2024-04-12 19:23:26 +0330

    add a page that shows the authentication page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
+import AuthPages from "./pages/AuthPages";
+
 function App() {
-  return <h1>پروژه دیوار</h1>;
+  return (
+    <div>
+      <h1>پروژه دیوار</h1>
+      <AuthPages />
+    </div>
+  );
 }
 
 export default App;
