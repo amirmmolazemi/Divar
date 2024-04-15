226e705bae6af442270bbf7b25999e76372eecf5
AmirMohammad Molazemi <amirmohamad.molazemi@gmail.com>
2024-04-15 16:12:52 +0330

    add a conditional for routers

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,24 +1,40 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import PageNotFound from "pages/404";
 import AdminPage from "pages/AdminPage";
 import AuthPages from "pages/AuthPages";
 import DashboardPage from "pages/DashboardPage";
 import HomePage from "pages/HomePage";
+import Loader from "components/modules/Loader";
 import { getProfile } from "services/user.js";
 
 function Router() {
   const { data, isLoading, error } = useQuery(["profile"], getProfile);
   console.log({ data, isLoading, error });
 
-  if (isLoading) return <h1>Loading ...</h1>
+  if (isLoading) return <Loader />;
 
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="/auth" element={<AuthPages />} />
-      <Route path="/dashboard" element={<DashboardPage />} />
-      <Route path="/admin" element={<AdminPage />} />
+      <Route
+        path="/dashboard"
+        element={data ? <DashboardPage /> : <Navigate to="/auth" />}
+      />
+      <Route
+        path="/auth"
+        element={data ? <Navigate to="/dashboard" /> : <AuthPages />}
+      />
+      <Route
+        path="/admin"
+        element={
+          data && data.data.role === "ADMIN" ? (
+            <AdminPage />
+          ) : (
+            <Navigate to="/" />
+          )
+        }
+      />
       <Route path="*" element={<PageNotFound />} />
     </Routes>
   );
