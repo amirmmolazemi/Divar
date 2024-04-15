093f8fc3768116745093699e094160b565729ac0
AmirMohammad Molazemi <amirmohamad.molazemi@gmail.com>
2024-04-15 14:35:07 +0330

    use query to get the datas

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,11 +1,18 @@
 import { Route, Routes } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
 import PageNotFound from "pages/404";
 import AdminPage from "pages/AdminPage";
 import AuthPages from "pages/AuthPages";
 import DashboardPage from "pages/DashboardPage";
 import HomePage from "pages/HomePage";
+import { getProfile } from "services/user.js";
 
 function Router() {
+  const { data, isLoading, error } = useQuery(["profile"], getProfile);
+  console.log({ data, isLoading, error });
+
+  if (isLoading) return <h1>Loading ...</h1>
+
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
