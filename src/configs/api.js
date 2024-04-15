67ecfcf088138f353aec68f82d5679877b934d11
AmirMohammad Molazemi <amirmohamad.molazemi@gmail.com>
2024-04-15 16:14:40 +0330

    add a interceptor for response when we give response from server

--- src/configs/api.js
+++ src/configs/api.js
@@ -1,5 +1,7 @@
 import axios from "axios";
-import { getCookie } from "src/utils/cookie";
+import { getNewTokens } from "services/token";
+import { setCookie } from "src/utils/cookie";
+import { getCookie } from "utils/cookie";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
@@ -18,6 +20,22 @@ api.interceptors.request.use(
   },
   (error) => {
     return Promise.reject(error);
+  }
+);
+
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    const originalRequest = error.config;
+    if (error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      const response = await getNewTokens();
+      if (!response?.response) return;
+      setCookie(response.response.data);
+      return api(originalRequest);
+    }
   }
 );
 
