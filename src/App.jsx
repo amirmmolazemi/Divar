a8cf7325de8fae99cc3e6eb541f1295f7518a099
AmirMohammad Molazemi <amirmohamad.molazemi@gmail.com>
2024-09-16 22:31:22 +0330

    create the project structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,5 @@
-import Router from "./router/Router";
-
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { BrowserRouter } from "react-router-dom";
-
-import defaultOptions from "./configs/reactQuery";
-import Layout from "./layout/Layout";
-
-const queryClient = new QueryClient({ defaultOptions });
-
 function App() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <Layout>
-          <Router />
-        </Layout>
-      </BrowserRouter>
-      <ReactQueryDevtools />
-    </QueryClientProvider>
-  );
+  return <h1>salam</h1>;
 }
 
 export default App;
--- src/services/user.js
+++
@@ -1,4 +0,0 @@
-import api from "configs/api";
-
-const getProfile = () => api.get("user/whoami").then((res) => res || false);
-export { getProfile };
