b2c096e710856b6167252ab2fee3ad2db7c84d02
AmirMohammad Molazemi <amirmohamad.molazemi@gmail.com>
2024-09-17 16:46:47 +0330

    feat: configure router dom and react query

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter } from "react-router-dom";
+import Router from "router/Router";
+import defaultOptions from "configs/reactQuery";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
 function App() {
-  return <h1>salam</h1>;
+  const queryClient = new QueryClient({ defaultOptions });
+  return (
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
