10f2852d3af1fb74675584ce68614810435b2320
AmirMohammad Molazemi <amirmohamad.molazemi@gmail.com>
2024-09-17 16:47:47 +0330

    feat: create routes with role validation for access

---
+++ src/router/Router.jsx
@@ -0,0 +1,44 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import AuthPage from "pages/AuthPage";
+import HomePage from "pages/HomePage";
+import DashboardPage from "pages/DashboardPage";
+import AdminPage from "pages/AdminPage";
+import PageNotFound from "pages/404";
+import { useQuery } from "@tanstack/react-query";
+import { getProfile } from "services/user";
+import Loader from "components/modules/Loader";
+
+function Router() {
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["profile"],
+    queryFn: getProfile,
+  });
+  if (isLoading) return <Loader />;
+  if (error) return <h1>error ...</h1>;
+  return (
+    <Routes>
+      <Route index element={<HomePage />} />
+      <Route
+        path="/dashboard"
+        element={data ? <DashboardPage /> : <Navigate to="/auth" />}
+      />
+      <Route
+        path="/auth"
+        element={data ? <Navigate to="/dashboard" /> : <AuthPage />}
+      />
+      <Route
+        path="/admin"
+        element={
+          data && data.data.role === "ADMIN" ? (
+            <AdminPage />
+          ) : (
+            <Navigate to="/" />
+          )
+        }
+      />
+      <Route path="*" element={<PageNotFound />} />
+    </Routes>
+  );
+}
+
+export default Router;
